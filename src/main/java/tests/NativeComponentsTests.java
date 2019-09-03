05fca8421657a7cc6c28c8663102981efb638f18
Gerry Sousa <gerry.nett@gmail.com>
2019-09-02 21:44:21 -0300

    OK - Teste para manipular Content Scrolling

--- src/main/java/tests/NativeComponentsTests.java
+++ src/main/java/tests/NativeComponentsTests.java
@@ -4,15 +4,19 @@ import base.BaseTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.*;
+import  utils.Steps.*;
 
 public class NativeComponentsTests extends BaseTests {
     private HomePage homePage;
     private MenuPage menuPage;
     NativeComponentsPage nativeComponentsPage;
     NativeComponentsImageCollectionPage nativeComponentsImageCollectionPage;
+    NativeComponentsSteps nativeComponentsSteps;
+    NativeComponentsContentScrollingPage nativeComponentsContentScrollingPage;
 
-    @Test(priority = 1, description = "Teste para manipular Alert")
-    public void Test_MensagemAlerta() {
+
+    @Test(priority = 1, description = "Teste para acessar Image Collection")
+    public void Test_AcessarImageCollection() {
         homePage = new HomePage();
         menuPage = new MenuPage();
         nativeComponentsPage= new NativeComponentsPage();
@@ -27,6 +31,21 @@ public class NativeComponentsTests extends BaseTests {
 
         String textoTituloImagemColletion = "Image Collection";
         String teste2 = nativeComponentsImageCollectionPage.obterTextoTitulo();
+        nativeComponentsImageCollectionPage.tirarScreenShotDaTela();
+
+        Assert.assertEquals(teste2, textoTituloImagemColletion);
+    }
+
+    @Test(priority = 1, description = "Teste para acessar Content Scrolling")
+    public void Test_AcessarContentScrolling() {
+        nativeComponentsSteps = new NativeComponentsSteps();
+        nativeComponentsContentScrollingPage = new NativeComponentsContentScrollingPage();
+
+        nativeComponentsSteps.acessarContentScrollingPage();
+
+        String textoTituloImagemColletion = "Content Scrolling";
+        String teste2 = nativeComponentsContentScrollingPage.obterTextoTitulo();
+        nativeComponentsContentScrollingPage.tirarScreenShotDaTela();
 
         Assert.assertEquals(teste2, textoTituloImagemColletion);
     }
